2efb7cb82c1f355c8a9c9865e8644bffc0541501
Grant Nelson <grantnelson1@gmail.com>
2021-12-22 19:04:08 -0700

    Update MainForm.Designer.cs

--- PetiteParser/ExamplesRunner/MainForm.Designer.cs
+++ PetiteParser/ExamplesRunner/MainForm.Designer.cs
@@ -133,6 +133,7 @@ namespace ExamplesRunner {
             // 
             // codeColoringBox
             // 
+            this.codeColoringBox.AcceptsTab = true;
             this.codeColoringBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
