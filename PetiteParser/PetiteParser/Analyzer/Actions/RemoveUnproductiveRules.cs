47b5498a026bd3e226fd60057dfc8acbd3c3b08e
Grant Nelson <grantnelson1@gmail.com>
2023-02-27 08:26:55 -0700

    Updating

--- PetiteParser/PetiteParser/Analyzer/Actions/RemoveUnproductiveRules.cs
+++ PetiteParser/PetiteParser/Analyzer/Actions/RemoveUnproductiveRules.cs
@@ -1,33 +1,32 @@
 ﻿using PetiteParser.Misc;
 using System.Linq;
 
-namespace PetiteParser.Analyzer.Actions {
+namespace PetiteParser.Analyzer.Actions;
 
-    /// <summary>An action to remove any unproductive rules from the grammar.</summary>
-    internal class RemoveUnproductiveRules : IAction {
+/// <summary>An action to remove any unproductive rules from the grammar.</summary>
+internal class RemoveUnproductiveRules : IAction {
 
-        /// <summary>Performs this action on the given grammar.</summary>
-        /// <param name="analyzer">The analyzer to perform this action on.</param>
-        /// <param name="log">The log to write notices, warnings, and errors.</param>
-        /// <returns>True if the grammar was changed.</returns>
-        public bool Perform(Analyzer analyzer, Logger.Log log) =>
-            analyzer.Grammar.Terms.ForeachAny(term => removeUnproductiveRules(term, log));
+    /// <summary>Performs this action on the given grammar.</summary>
+    /// <param name="analyzer">The analyzer to perform this action on.</param>
+    /// <param name="log">The log to write notices, warnings, and errors.</param>
+    /// <returns>True if the grammar was changed.</returns>
+    public bool Perform(Analyzer analyzer, Logger.Log log) =>
+        analyzer.Grammar.Terms.ForeachAny(term => removeUnproductiveRules(term, log));
 
-        /// <summary>Removes unproductive rules from the given term.</summary>
-        /// <param name="term">The term to remove unproductive rules from.</param>
-        /// <param name="log">The log to write notices, warnings, and errors.</param>
-        /// <returns>True if any rules were removed, false if no rules were removed.</returns>
-        static private bool removeUnproductiveRules(Grammar.Term term, Logger.Log log) {
-            int count = term.Rules.RemoveAll(unproductiveRule);
-            if (count <= 0) return false;
-            log?.AddNotice("Removed {0} unproductive rules from {1}.", count, term);
-            return true;
-        }
-
-        /// <summary>True if the rule is a simple left recursion rule which performs no production.</summary>
-        /// <param name="rule">The rule to check.</param>
-        /// <example>Look for a rule like "T := T".</example>
-        static private bool unproductiveRule(Grammar.Rule rule) =>
-            (rule.BasicItems.Count() == 1) && (rule.BasicItems.First() as Grammar.Term == rule.Term);
+    /// <summary>Removes unproductive rules from the given term.</summary>
+    /// <param name="term">The term to remove unproductive rules from.</param>
+    /// <param name="log">The log to write notices, warnings, and errors.</param>
+    /// <returns>True if any rules were removed, false if no rules were removed.</returns>
+    static private bool removeUnproductiveRules(Grammar.Term term, Logger.Log log) {
+        int count = term.Rules.RemoveAll(unproductiveRule);
+        if (count <= 0) return false;
+        log?.AddNotice("Removed {0} unproductive rules from {1}.", count, term);
+        return true;
     }
+
+    /// <summary>True if the rule is a simple left recursion rule which performs no production.</summary>
+    /// <param name="rule">The rule to check.</param>
+    /// <example>Look for a rule like "T := T".</example>
+    static private bool unproductiveRule(Grammar.Rule rule) =>
+        (rule.BasicItems.Count() == 1) && (rule.BasicItems.First() as Grammar.Term == rule.Term);
 }
--- PetiteParser/PetiteParser/Logger/Log.cs
+++ PetiteParser/PetiteParser/Logger/Log.cs
@@ -11,20 +11,20 @@ namespace PetiteParser.Logger {
         private readonly Queue<Entry> entries;
 
         /// <summary>Creates a new inspector argument.</summary>
-        public Log() => entries = new();
+        public Log() => this.entries = new();
 
         /// <summary>Removes all the entries from the logs.</summary>
-        public void Clear() => entries.Clear();
+        public void Clear() => this.entries.Clear();
 
         /// <summary>Indicates that at least one error has occurred.</summary>
-        public bool Failed => entries.Any(e => e.Level == Level.Error);
+        public bool Failed => this.entries.Any(e => e.Level == Level.Error);
 
         /// <summary>Gets all the log entries.</summary>
-        public IEnumerable<Entry> Entries => entries;
+        public IEnumerable<Entry> Entries => this.entries;
 
         /// <summary>Adds the given entry to the logs.</summary>
         /// <param name="entry">The entry to add to the log.</param>
-        public void Add(Entry entry) => entries.Enqueue(entry);
+        public void Add(Entry entry) => this.entries.Enqueue(entry);
 
         /// <summary>Logs an entry to the log at the given level.</summary>
         /// <param name="level">The level of the entry to log.</param>
