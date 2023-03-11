d3b02a6492cd09d0cfa6bd42e6baf7584a69f4bc
Grant Nelson <grantnelson1@gmail.com>
2023-03-11 09:15:26 -0700

    Update Normalizer.cs

--- PetiteParser/PetiteParser/Grammar/Normalizer/Normalizer.cs
+++ PetiteParser/PetiteParser/Grammar/Normalizer/Normalizer.cs
@@ -19,14 +19,14 @@ static public class Normalizer {
         // Remove unneeded parts of the grammar and sort the rules to make finding duplicates easier.
         new RemoveUnusedTerms(),
         new RemoveUnproductiveRules(),
-        //new RemoveMonoproductiveTerms(), // TODO: Enable
+        new RemoveMonoproductiveTerms(),
         new SortRules(),
         new RemoveDuplicateRules(),
         new RemoveDuplicateTerms(),
 
         // Change the rules to be biased towards shifts over reduces.
-        //new InlineOneRuleTerms(), // TODO: Enable
-        //new InlineTails(), // TODO: Enable
+        new InlineOneRuleTerms(),
+        new InlineTails(),
 
         // More complex precepts are run last so that unproductive
         // rules and any complications have already been removed.
